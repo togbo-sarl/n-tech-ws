fb3c0f2ae99b8e4942b8c06a63a658433b1668f4
Hodonou Sounton <sounton@gmail.com>
2025-02-17 01:03:15 +0100

    Add hamburger

--- app/page.js
+++ app/page.js
@@ -1,5 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import Hamburger from "./ui/Hamburger";
+import Xmark from "./ui/XMark";
+
 function Home() {
-  return <h1 className="text-2xl">Home</h1>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <header className="flex flex-col h-screen">
+        <nav className="mx-3 my-4 flex justify-between items-center">
+          <span>Logo</span>
+
+          <span onClick={handleMenuClick}>
+            {isMenuOpen ? (
+              <Xmark className="w-8" />
+            ) : (
+              <Hamburger className="w-8" />
+            )}
+          </span>
+        </nav>
+        <div className="flex-1 bg-teal-600">Hero</div>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default Home;
